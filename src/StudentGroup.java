49af658121b3c80b468c35338950efc5e03b4705
nagaboinarajendra <nagabhoinarajendra@gmail.com>
2017-09-16 14:37:08 +0530

    updated code

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -32,19 +32,32 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
-		if(students == null)  
-          throw new IllegalArgumentException();  
+		if(students == null){  
+          throw new IllegalArgumentException();
+          }  
 	}
 
 	@Override
 	public Student getStudent(int index) {
 		// Add your implementation here
+		if(index<0 || index>=students.length){
+			throw new IllegalArgumentException();
+		}
 		return null;
 	}
 
 	@Override
 	public void setStudent(Student student, int index) {
 		// Add your implementation here
+		if(index<0 || index>=students.length){
+			throw new IllegalArgumentException();
+		}
+		else if(students == null){  
+          throw new IllegalArgumentException();
+        } 
+        else{
+        	students[index] = student;
+        }
 	}
 
 	@Override
